9cc5fc7b07b80374a5e78802dc5ea11865ebde15
hans <hans.krebs@me.com>
2021-10-21 13:06:43 +0200

    Add proper page title.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import Head from 'next/head';
+
 export default function Home() {
-  return <h1>Foo Bar from Home!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Shortly - More than just shorter links</title>
+      </Head>
+      <h1>Foo Bar from Home!</h1>
+    </>
+  );
 }
