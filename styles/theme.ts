3795e439347937cde734bd107c470e1f5920c0fb
hans <hans.krebs@me.com>
2021-10-21 13:19:41 +0200

    Add styles & themes boilerplate.

---
+++ styles/theme.ts
@@ -0,0 +1,30 @@
+const theme = {
+  typography: {
+    h1: '2rem',
+    h2: '1.3rem',
+    h3: '1.2rem',
+    p: '1.125rem', // 18px
+    label: '1rem',
+  },
+  palette: {
+    primary: {
+      cyan: 'hsl(180, 66%, 49%)',
+      darkViolet: 'hsl(257, 27%, 26%)',
+    },
+    secondary: {
+      red: 'hsl(0, 87%, 67%)',
+    },
+    neutral: {
+      gray: 'hsl(0, 0%, 75%)',
+      grayishViolet: 'hsl(257, 7%, 63%)',
+      veryDarkBlue: 'hsl(255, 11%, 22%)',
+      veryDarkViolet: 'hsl(260, 8%, 14%)',
+    },
+  },
+  borderRadius: '5px',
+  paddingTB: '0.5rem 0',
+  paddingTBLR: '0.5rem 0.5rem',
+  margin: '0.5rem 0',
+};
+
+export default theme;
