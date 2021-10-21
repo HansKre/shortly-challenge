c50a4d06d13eb3df9992c8f70340f178602f823a
hans <hans.krebs@me.com>
2021-10-21 15:01:16 +0200

    Wrap all pages with layout-container.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  padding: 15px 11.5vw;
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
